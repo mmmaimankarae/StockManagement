aec04c54ee79c979f00a108905ef818390379a2d
mmmaimankarae <mineteacha@hotmail.com>
2024-02-06 12:47:22 +0700

    1'st login

---
+++ login.php
@@ -0,0 +1,17 @@
+<?php 
+    $msconnect = mysqli_connect("localhost", "root", "", "myStore");
+    $userName = $_POST['username'];
+    $password = $_POST['password'];
+    $msquery = "SELECT * FROM CUSTOMER_ACCOUNT WHERE UserName = '$userName' AND Password = '$password';";
+    $msresults = mysqli_query($msconnect, $msquery);
+    if ($row = mysqli_fetch_array($msresults)) {
+        if ($userName == 'admin') {
+            header("Location: ./ShowProduct.php");
+        }
+        echo $userName;
+    } else {
+        include('./test.html');
+        echo "<div style='color: #ff0000; margin-top: 25%; position: absolute;'><br><h4>username หรือ password กรุณากรอกใหม่อีกครั้ง</h4> </div>";
+    }
+    mysqli_close($cx);
+?>
